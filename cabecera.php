ed9f082fbd538a78c403ef550ad5ed14693af6d7
Mauricio <mauricioaizaga@gmail.com>
2012-07-28 23:15:34 -0500

    Agregando Google Analitics

--- cabecera.php
+++ cabecera.php
@@ -34,3 +34,17 @@
 		<script type="text/javascript" src="<?=$url?>js/cycle.js"></script><!-- include Cycle plugin -->
 		<script type="text/javascript" src="<?=$url?>js/jquery.tools.min.js"></script>
 		<script type="text/javascript" src="<?=$url?>js/jquery-menu.js"></script>
+		
+		<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-25028369-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
